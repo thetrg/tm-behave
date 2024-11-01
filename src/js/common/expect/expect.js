1a425fa5aace815a0347d11124c5abb133aeb8a1
Ron Bravo <ronbravo1701@gmail.com>
2024-11-01 07:28:58 -0700

    WIP: Save

--- src/js/common/expect/expect.js
+++ src/js/common/expect/expect.js
@@ -10,12 +10,12 @@ function createExpectation (value) {
 
 function is (value) {
   if (this.value === value) { return }
-  throw new Error (`- value does NOT match what is expected`);
+  throw new Error ('value does NOT match what is expected');
 }
 
 function isNot (value) {
   if (this.value !== value) { return }
-  throw new Error (`- expected values are NOT supposed to match`);
+  throw new Error ('expected values are NOT supposed to match');
 }
 
 export const expect = createExpectation;
